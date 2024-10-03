c0965e67e4105a118157c59e0489980446017cdb
Marie ZINS <mzins@gaming.tech>
2024-10-03 15:37:03 +0200

    displaced function into BitUtils

--- src/steganography/BitUtils.h
+++ src/steganography/BitUtils.h
@@ -1,1 +1,21 @@
 #pragma once
+#define ENCRYPTSIGN 38977
+#define MAX16BITS 65535
+
+
+class BitUtils {
+public:
+	static void SetBits(byte value, byte numOfBits, byte* place);
+
+	static void SetBytes(unsigned int value, byte size, byte* place);
+
+	static void SetSignEncrypted(byte* place);
+
+	static byte ReadBits(byte numOfBits, byte* place);
+
+	static unsigned int ReadBytes(byte size, byte* place);
+
+	static bool CheckSignEncrypted(byte* place);
+
+
+};
--- src/steganography/Bitmap.cpp
+++ src/steganography/Bitmap.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Bitmap.h"
 #include "File.h"
+#include "BitUtils.h"
 
 Bitmap::Bitmap() {
 	m_buffer = nullptr;
@@ -29,7 +30,6 @@ void Bitmap::uninit() {
 	m_colorBits = nullptr;
 }
 
-
 HBITMAP Bitmap::GenerateHBitMap(HDC hdc) {
 	HBITMAP bmpTest = CreateDIBitmap(hdc, &m_bih, CBM_INIT, m_colorBits, (BITMAPINFO*)&m_bih, DIB_RGB_COLORS);
 	return bmpTest;
@@ -46,73 +46,6 @@ byte* Bitmap::GetBuffer() {
 	return m_buffer;
 }
 
-void Bitmap::SetBits(byte value, byte numOfBits, byte* place) {
-	byte mask = numOfBits * 2 - 1;
-	byte invertedMask = ~mask;
-	*place &= invertedMask;
-	value &= mask;
-	*place |= value;
-}
-
-byte Bitmap::ReadBits(byte numofBits, byte* place) {
-	byte mask = numofBits * 2 - 1;
-	return *place & mask;
-}
-
-void Bitmap::SetBytes(unsigned int value, byte size, byte* place) {
-	int offset = 0;
-	for (int i = 0, j = 0; i < size * 6 ; i++) {
-		byte tempStorage = 0b00;
-		if (j==2) {
-			tempStorage = ((byte)(value >> (i + offset)));
-			SetBits(tempStorage, 2, place);
-			offset++;
-			j = 0;
-		}
-		else {
-			tempStorage = ((byte)(value >> (i + offset)));
-			SetBits(tempStorage, 1, place);
-			j++;
-		}
-		place++;
-	}
-}
-
-unsigned int Bitmap::ReadBytes(byte size, byte* place) {
-	unsigned int returnValue = 0b00;
-	int offset = 0;
-	for (int i = 0, j = 0; i < size * 6; i++) {
-		byte getBits = 0b00;
-		unsigned int tempStorage = 0b00;
-		
-		if (j == 2) {
-			getBits = ReadBits(2, place);
-			tempStorage = (unsigned int)getBits << (i + offset);
-			offset++;
-			j = 0;
-			
-		}
-		else {
-			getBits = ReadBits(1, place);
-			tempStorage = (unsigned int)getBits << (i + offset);
-			j++;
-		}
-		returnValue |= tempStorage;
-		place++;
-	}
-	return returnValue;
-}
-
-void Bitmap::SetSignEncrypted() {
-	byte* place = m_colorBits;
-	unsigned int sign = ENCRYPTSIGN;
-	SetBytes(sign, 2, place);
-}
-void Bitmap::setTextHeader(unsigned int lengthOfText) {
-	byte* place = m_colorBits+12;
-	SetBytes(lengthOfText, 2, place);
-	
-}
 
 bool Bitmap::EncryptText(const char* text) {
 	unsigned int size = std::strlen(text);
@@ -123,13 +56,14 @@ bool Bitmap::EncryptText(const char* text) {
 			bits = m_bfh.bfSize - m_bfh.bfOffBits - 24;
 		}
 
-		setTextHeader(size);
+		BitUtils::SetBytes(size, 2, m_colorBits + 12);
 		byte* place = m_colorBits + 24;
 		for (int i = 0; i < size; i++) {
-			SetBytes((unsigned int)text[i], 2, place);
+			BitUtils::SetBytes((unsigned int)text[i], 2, place);
 			place += 6;
 		}
-		SetSignEncrypted();
+		place = m_colorBits;
+		BitUtils::SetSignEncrypted(place);
 		return true;
 	}
 	else {
@@ -137,26 +71,12 @@ bool Bitmap::EncryptText(const char* text) {
 	}
 }
 
-bool Bitmap::CheckSignEncrypted() {
-	byte* place = m_colorBits;
-	unsigned int sign = ENCRYPTSIGN;
-	unsigned int sign2 = (unsigned int)ReadBytes(2, place);
-	if (sign2 == sign) {
-		return true;
-	}
-	return false;
-}
-
-unsigned int Bitmap::ReadTextHeader() {
-	byte* place = m_colorBits + 12;
-	return ReadBytes(2, place);
-}
-
-const char* Bitmap::ReadEncryptedText(int textLength) {
+const char* Bitmap::ReadEncryptedText() {
+	unsigned int textLength = BitUtils::ReadBytes(2, m_colorBits + 12);
 	char* returnArray = new char[textLength+1];
 	byte* place = m_colorBits + 24;
 	for (int i = 0; i < textLength; i++) {
-		returnArray[i] = (char)ReadBytes(1, place);
+		returnArray[i] = (char)BitUtils::ReadBytes(1, place);
 		place += 6;
 	}
 	returnArray[textLength] = '\0';
